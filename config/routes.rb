caa847f7a1634e49617d782d2dfd452d8726ba75
Junpei1121 <momooooonnn@gmail.com>
2023-12-06 09:02:24 +0000

    devise導入、モデル定義、ルーティング設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,49 @@
 Rails.application.routes.draw do
+  # ユーザー用デバイス
+# URL /customers/sign_in ...
+devise_for :users, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用デバイス
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+get "search" => "searches#search"
+
+#管理者
+namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+  end
+
+#ユーザー
+scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about', as: 'about'
+    resources :users, only: [:index, :edit, :update, :show] do
+    collection do
+      get :withdraw_confirm
+      patch :withdraw
+    end
+    member do
+      get :favorites
+      get :follows, :followers
+    end
+    resource :relationships, only: [:create, :destroy]
+  end
+
+  end
+    resources :posts, only: [:new, :create, :index, :edit, :update, :destroy, :show] do
+      resource :favorite, only: [:create, :destroy]
+      resources :comments, only: [:create,:destroy]
+      collection do
+          get :search_tag
+      end
+
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
