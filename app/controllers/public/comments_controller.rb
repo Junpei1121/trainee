63cc0b75389c3aabc667bf7ca8056421129df203
Junpei1121 <momooooonnn@gmail.com>
2023-12-24 08:26:32 +0000

    レスポンシブ対応、seedファイル追記

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,11 +1,4 @@
 class Public::CommentsController < ApplicationController
-  # def create
-  #   @post = Post.find(params[:post_id])
-  #   comment = current_user.comments.new(comment_params)
-  #   comment.post_id = @post.id
-  #   comment.save
-  # end
-  
   def create
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.new(comment_params)
@@ -19,9 +12,7 @@ class Public::CommentsController < ApplicationController
       end
     end
   end
-  
-  
-  
+
   def destroy
     @post = Post.find(params[:post_id])
     Comment.find(params[:id]).destroy
